84d991a48fbde1edbc74b33f00ef05a264fc3ec4
DGking <718731458@qq.com>
2018-05-16 18:22:46 +0800

    分销报备系统（移动端）

---
+++ fenxiao/js/common.js
@@ -0,0 +1,16 @@
+/*移动端自动计算长度*/
+$(function() {
+	$(window).resize(infinite);
+	function infinite() {
+		var htmlWidth = $('html').width();
+		if (htmlWidth >= 640) {
+			$('html').css({
+				"font-size" : "24px"
+			});
+		} else {
+			$('html').css({
+				"font-size" : 24 / 640 * htmlWidth + "px"
+			});
+		}
+	}infinite();
+});
